52a49b54b27bbd913050e90e87e9f39ff1dca729
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-16 21:45:16 +0500

    updated changes

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let [name, setName] = useState('Bilal')
+
   return (
     <View style={styles.container}>
-      <Text>Hello My World Nijah</Text>
-      <Text>Hello Irfan</Text>
-      <Text>Hello Nijah</Text>
+     <Text>{name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
